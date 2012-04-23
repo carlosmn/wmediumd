f57399d972162f5fd7df703d434dde45d99dc996
Carlos Martín Nieto <carlos@cmartin.tk>
2012-04-23 16:51:59 +0200

    Update the test code

--- wmediumd/test.c
+++ wmediumd/test.c
@@ -2,19 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <zmq.h>
+#include "mac_address.h"
 
 int main(int argc, char **argv)
 {
-	puts("Just started");
-
 	void *context = zmq_init(1);
 	void *zsock = zmq_socket(context, ZMQ_REQ);
-	zmq_connect(zsock, "tcp://10.0.2.2:5555");
+	zmq_connect(zsock, "tcp://localhost:5555");
 	zmq_msg_t req, reply;
+	const char rnd[] = "pretend I'm random data";
+	struct mac_address addr;
 
 	while(1){
-		zmq_msg_init_size(&req, 13);
-		memcpy(zmq_msg_data(&req), "400000000000", 13);
+		/* Source */
+		zmq_msg_init_size(&req, sizeof(struct mac_address));
+		addr = string_to_mac_address("40:00:00:00:00:00");
+		memcpy(zmq_msg_data(&req), &addr, sizeof(struct mac_address) );
+		zmq_send(zsock, &req, ZMQ_SNDMORE);
+
+		/* Destination */
+		zmq_msg_init_size(&req, sizeof(struct mac_address));
+		addr = string_to_mac_address("40:00:00:00:10:00");
+		memcpy(zmq_msg_data(&req), &addr, sizeof(struct mac_address) );
+		zmq_send(zsock, &req, ZMQ_SNDMORE);
+
+		/* Data */
+		zmq_msg_init_size(&req, sizeof(rnd));
+		memcpy(zmq_msg_data(&req), rnd, sizeof(rnd));
 		zmq_send(zsock, &req, 0);
 		zmq_msg_close(&req);
 
