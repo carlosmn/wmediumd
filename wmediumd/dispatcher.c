123441512ffe636d48d3187390062e2c28592b98
Carlos Martín Nieto <cmn@dwim.me>
2013-02-25 23:56:03 +0100

    Remove 0MQ from the dispatcher
    The daemon still has it as a way to remember where to put the sending
    later on.

--- wmediumd/dispatcher.c
+++ wmediumd/dispatcher.c
@@ -1,4 +1,3 @@
-#include <zmq.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,30 +20,6 @@ static long int processed;
 static long int processed_size;
 
 static void *s_ctl;
-
-void exit_handler(int signal)
-{
-	struct timeval end;
-	long int diff;
-	zmq_msg_t msg;
-
-	if (gettimeofday(&end, NULL)){
-		perror("gettimeofday");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Figure out how long we ran */
-	diff = end.tv_sec - tv.tv_sec;
-	fprintf(stderr, "Ran for %d seconds\n", diff);
-	fprintf(stderr, "Average %ld xfer/sec\n", processed/diff);
-	fprintf(stderr, "Average %ld bytes/sec\n", processed_size/diff);
-
-	zmq_msg_init_data(&msg, CTL_BYE, strlen(CTL_BYE), NULL, NULL);
-	zmq_send(s_ctl, &msg, 0);
-	zmq_close(&msg);
-
-	exit(EXIT_SUCCESS);
-}
 
 int main(int argc, char **argv)
 {
@@ -98,94 +73,4 @@ int main(int argc, char **argv)
 			/* rand() and send the messages we need to */
 		}
 	}
-}
-
-int xmain(int argc, char **argv)
-{
-	void *ctx, *s, *s_pub;
-	char mac[18] = {0};
-
-	ctx = zmq_init(1);
-	/* Receives messages from the interfaces */
-	s = zmq_socket(ctx, ZMQ_PULL);
-	zmq_bind(s, "tcp://*:5555");
-	/* Sends messages to the interfaces */
-	s_pub = zmq_socket(ctx, ZMQ_PUB);
-	zmq_bind(s_pub, "tcp://*:5556");
-	/* Sends control information to the interfaces */
-	s_ctl = zmq_socket(ctx, ZMQ_PUSH);
-	zmq_bind(s, "tcp://*:5557");
-
-	memset(&jam_cfg, 0, sizeof(jam_cfg));
-	load_config("dispatcher-config.cfg");
-
-	if (gettimeofday(&tv, NULL)) {
-		perror("gettimeofday");
-		return EXIT_FAILURE;
-	}
-
-	if (signal(SIGINT, exit_handler) == SIG_ERR) {
-		perror("signal");
-		return EXIT_FAILURE;
-	}
-
-	while (1) {
-		zmq_msg_t msg, dst, src;
-		int64_t more;
-		size_t more_size;
-
-		zmq_msg_init(&src);
-		zmq_recv(s, &src, 0);
-
-		mac_address_to_string(mac, zmq_msg_data(&src));
-
-		printf("Frame %s ", mac);
-
-		more = 0;
-		more_size = sizeof(more);
-		zmq_getsockopt(s, ZMQ_RCVMORE, &more, &more_size);
-
-		if (!more) {
-			/* We should receive three parts. Something went wrong */
-			char *str = "ERROR: Destination is missing";
-			fputs(str, stderr);
-			continue;
-		}
-
-		/* Grab the scond part which is the destination */
-		zmq_msg_init(&dst);
-		zmq_recv(s, &dst, 0);
-		mac_address_to_string(mac, zmq_msg_data(&dst));
-		printf("-> %s\n", mac);
-
-		more = 0;
-		more_size = sizeof(more);
-		zmq_getsockopt(s, ZMQ_RCVMORE, &more, &more_size);
-
-		if (!more) {
-			/* We should receive three parts. Something went wrong */
-			char *str = "ERROR: Message is missing";
-			fputs(str, stderr);
-			continue;
-		}
-
-		zmq_msg_init(&msg);
-		zmq_recv(s, &msg, 0);
-		processed_size += zmq_msg_size(&msg);
-
-		zmq_send(s_pub, &dst, ZMQ_SNDMORE); /* For the SUB filter */
-		zmq_send(s_pub, &src, ZMQ_SNDMORE);
-		zmq_send(s_pub, &msg, 0);
-
-		zmq_msg_close(&src);
-		zmq_msg_close(&dst);
-		zmq_msg_close(&msg);
-
-		processed++;
-	}
-
-	zmq_close(s);
-	zmq_term(ctx);
-
-	return EXIT_SUCCESS;
 }
