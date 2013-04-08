6c435f661799b1393eb6414bc92516d2698f01ea
Carlos Martín Nieto <cmn@dwim.me>
2013-04-08 15:25:25 +0200

    Generate the config before starting dispatcher

--- wmediumd/bench.py
+++ wmediumd/bench.py
@@ -7,12 +7,22 @@ import time
 clients = []
 dispatcher = None
 bench_time = 15
+num_clients = 6
+loss_prob = 0.0
+
+# Write out the configuration for the dispatcher
+
+write_config = subprocess.Popen(["./write-config", "-l", str(loss_prob), "-n", str(num_clients),
+                                 "-o", "dispatcher-config.cfg"], stdout=subprocess.PIPE)
+write_config.communicate() # wait for it to finish
+
 
 dispatcher = subprocess.Popen(["./dispatcher"])
 print("Giving the dispatcher time to set up")
 time.sleep(1)
 
-for i in range(4):
+
+for i in range(num_clients):
     mac = "42:00:00:00:%02d:00" % i # wmediumd uses decimal for some reason
     print("Starting test for %s" % mac)
     clients.append(subprocess.Popen(["./test", "-t", "1", "-s", mac], stdout=subprocess.PIPE))
