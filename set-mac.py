a2c295c8665f0725912afe3ac83e4a12d6ff6f59
Carlos Martín Nieto <cmn@dwim.me>
2013-05-16 10:51:57 +0200

    Create script to set parameters based on the kernel command line

---
+++ set-mac.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import subprocess, re
+
+# Load the kernel module
+res = subprocess.call(["modprobe", "mac80211_hwsim", "radios=1"])
+if res != 0:
+    print("boo!")
+
+cmdline = open("/proc/cmdline").read()
+
+# Match the wmediumd.iface=wlan1 option
+pattern = re.compile(r'.*wmediumd\.iface=(.*)\s')
+iface = pattern.match(cmdline).group(1)
+
+# Match the wmediumd.addr
+pattern = re.compile(r'.*wmediumd\.addr=(.*)\s')
+addr = pattern.match(cmdline).group(1)
+
+# Match the dispatcher's address
+pattern = re.compile(r'.*wmediumd\.dispatcher=(.*)\s')
+disp = pattern.match(cmdline).group(1)
+
+# Set the MAC address for our virtual wireless interface
+res = subprocess.call(["ip", "link", "set", "dev", iface, "address", addr])
+if res != 0:
+    print("Error setting the MAC address")
+
+res = subprocess.call(["iw", "dev", iface, "set", "type", "ibss"])
+if res != 0:
+    print("Error setting IBSS mode")
+
+# Now we can start wmediumd
+
+subprocess.call(["wmediumd", "-d", disp, "-a", addr, "-b"])
+if res != 0:
+    print("Error starting wmediumd")
+
