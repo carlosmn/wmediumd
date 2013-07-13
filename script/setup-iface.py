1b116287269225785a08f25f0ef0e599b54de7fd
Carlos Martín Nieto <cmn@dwim.me>
2013-07-14 00:16:38 +0200

    Fix up iface creation

--- script/setup-iface.py
+++ script/setup-iface.py
@@ -3,6 +3,9 @@
 import re, shlex
 from subprocess import call
 
+def run(str):
+    call(shlex.split(str))
+
 IFACE = "mesh0"
 
 cmdline = open("/proc/cmdline").read()
@@ -10,23 +13,27 @@ cmdline = open("/proc/cmdline").read()
 pattern = re.compile(r'.*wmediumd\.id=(.*)\s')
 id = pattern.match(cmdline).group(1)
 
-add_iface = "iw dev wlan0 interface add %s type ibss" % IFACE
-call(shlex.split(add_iface))
+# Start by adding an IP address to eth0 so we can talk to the
+# dispatcher
+run("ip address add 172.16.0.%s/12 dev eth0" % id)
+run("ip link set eth0 up")
 
 # Load the kernel module
-call(shlex.split("modprobe mac80211"))
+run("modprobe mac80211")
 insmod = "insmod /wmediumd/mac80211_hwsim.ko radios=1 mac_offset=%s" % id
-call(shlex.split(insmod))
+run(insmod)
 
-# The address should already be fine, and we can't change it anyway
-#set_mac = "ip link set dev %s address 42:00:00:00:%02x:00" % IFACE, int(id)
-#call(shlex.split(set_mac))
+# Create the interface over which the nodes will talk to each other,
+# join it to "testnet" with a particular BSSID and give it its IP
+# address for the wireless network
+add_iface = "iw dev wlan0 interface add %s type ibss" % IFACE
+run(add_iface)
 
 iface_up = "ip link set %s up" % IFACE
-call(shlex.split(iface_up))
+run(iface_up)
 
 join_net = "iw dev %s ibss join testnet 2412 26:1C:41:D1:AC:90" % IFACE
-call(shlex.split(join_net))
+run(join_net)
 
 set_ip = "ip address add 192.168.2.%s/24 dev %s" % (id, IFACE)
-call(shlex.split(join_net))
+run(set_ip)
