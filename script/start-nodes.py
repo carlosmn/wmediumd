5c839ef588fa2e9f11be02ddef494f0e6b9caef9
Carlos Martín Nieto <cmn@dwim.me>
2013-07-16 13:44:21 +0200

    Run the dispatcher from start-nodes.py

--- script/start-nodes.py
+++ script/start-nodes.py
@@ -2,7 +2,7 @@
 
 # Start a number of nodes which all talk to the same dispatcher
 
-import psutil, os
+import os
 import subprocess, shlex, signal
 from optparse import OptionParser
 
@@ -11,6 +11,8 @@ BASE_IMAGE = BASE_PATH + "wmediumd-skel"
 KERNEL = BASE_PATH + "vmlinuz-3.8-2-amd64"
 INITRD = BASE_PATH + "initrd.img-3.8-2-amd64"
 OPTIONS = "root=UUID=a9468c72-4a4a-4463-aa25-5596f9b951b7 ro quiet"
+
+dispatcher = 0
 
 def start_one(n): 
     wmd_opts = "wmediumd.dispatcher=192.168.2.1 wmediumd.id=%d" % n
@@ -23,10 +25,8 @@ def start_one(n):
     return subprocess.Popen(command)
 
 def rewrite_config(loss):
-    cmd = shlex.split("../wmediumd/write-config -n 50 -o ../wmediumd/dispatcher-config.cfg -l %s" % loss)
-    subprocess.call(cmd, stderr=None)
-    pid = [ps.pid for ps in psutil.get_process_list() if ps.name == "dispatcher"]
-    os.kill(pid[0], signal.SIGHUP)
+    cmd = shlex.split("../wmediumd/write-config -n 50 -o dispatcher-config.cfg -l %s" % loss)
+    subprocess.call(cmd, stdout=None, stderr=None)
 
 def run(str):
     subprocess.call(shlex.split(str))
@@ -52,10 +52,11 @@ try:
         command = raw_input("> ")
         if command.startswith("loss"):
             rewrite_config(command[5:])
+            dispatcher.send_signal(signal.SIGHUP)
         else:
             print("Unkown command")
-except:
-    pass
+except Exception as e:
+    print e
 
 # Bring down the bridge
 run("ip link set br-wmediumd down")
@@ -64,3 +65,5 @@ run("brctl delbr br-wmediumd")
 # And kill the virtual machines
 for node in nodes:
     node.send_signal(signal.SIGINT)
+
+dispatcher.send_signal(signal.SIGINT)
